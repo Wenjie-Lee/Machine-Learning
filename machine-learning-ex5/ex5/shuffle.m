c7cc6c73eb6bb532460ec50600a7899e71885140
Lee-kraken <youzhaxiaoniqiu@gmail.com>
2020-09-17 17:42:52 +0800

    ex5 shuffle modification
    2020/9/17

--- machine-learning-ex5/ex5/shuffle.m
+++ machine-learning-ex5/ex5/shuffle.m
@@ -1,16 +1,12 @@
 %% shuffle: shuffle the dataset(X, y)
 function [Xs, ys] = shuffle(X, y)
-
     m = size(X, 1);
 
     Xs = X;
     ys = y;
     for i = 1:m
-<<<<<<< Updated upstream
-        k = round(rand() * m);
-=======
+        % k = round(rand() * m);
         k = round(rand() * (m-1)) + 1;
->>>>>>> Stashed changes
         Xs(i, :) = Xs(k, :);
         Xs(k, :) = X(i, :);
         ys(i) = ys(k);
