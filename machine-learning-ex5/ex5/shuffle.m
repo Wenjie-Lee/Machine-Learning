b989e6840da67633a5811631bcec86522bbd79bf
Lee-kraken <youzhaxiaoniqiu@gmail.com>
2020-09-17 17:44:59 +0800

    ex5 shuffle align modification
    Octave function align rectification

--- machine-learning-ex5/ex5/shuffle.m
+++ machine-learning-ex5/ex5/shuffle.m
@@ -1,16 +1,16 @@
 %% shuffle: shuffle the dataset(X, y)
 function [Xs, ys] = shuffle(X, y)
-    m = size(X, 1);
+m = size(X, 1);
 
-    Xs = X;
-    ys = y;
-    for i = 1:m
-        % k = round(rand() * m);
-        k = round(rand() * (m-1)) + 1;
-        Xs(i, :) = Xs(k, :);
-        Xs(k, :) = X(i, :);
-        ys(i) = ys(k);
-        ys(k) = y(i);
-    end
+Xs = X;
+ys = y;
+for i = 1:m
+    % k = round(rand() * m);
+    k = round(rand() * (m-1)) + 1;
+    Xs(i, :) = Xs(k, :);
+    Xs(k, :) = X(i, :);
+    ys(i) = ys(k);
+    ys(k) = y(i);
+end
 
 end
